1ddcc08cf3a48444512dee8ec3a9bbd385698761
Hosam Abuelhiga <102473217+hosam10@users.noreply.github.com>
2024-12-13 17:06:03 +0200

    Update App.java

--- exe_2_QA_2024/src/main/java/exe_2_QA_2024/exe_2_QA_2024/App.java
+++ exe_2_QA_2024/src/main/java/exe_2_QA_2024/exe_2_QA_2024/App.java
@@ -1,7 +1,7 @@
 package exe_2_QA_2024.exe_2_QA_2024;
 
 /**
- * Hello world!
+ * Hello world! hosam Abuelhiga - 322507880  -  mohamed saleem - 207179011
  */
 public class App {
     public static void main(String[] args) {
